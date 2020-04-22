ace7afa67e125cbcf281697ddaaf0c174066cddc
Youngsdg1 <52898735+Youngsdg1@users.noreply.github.com>
2020-04-22 19:40:09 +0900

    0422 | EDIT api | YJ

--- Back-End/whyEat/api/serializers.py
+++ Back-End/whyEat/api/serializers.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
 from .models import User, User_history
+from django.contrib.auth import get_user_model
 # from django.contrib.auth.models import User
-
+User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('email', 'name', 'nickname')
 
 class UserCreationSerializer(serializers.ModelSerializer):
 
--- Back-End/whyEat/api/views.py
+++ Back-End/whyEat/api/views.py
@@ -9,7 +9,12 @@ from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
 from django.db.models import Sum
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny 
+from rest_framework.permissions import AllowAny
+
+from django.conf import settings
+from django.contrib.auth import login, logout
+from rest_framework import views, generics, response, permissions, authentication
+from .serializers import UserSerializer
 
 # class UserView(viewsets.ModelViewSet):
 #     queryset = User.objects.all()
@@ -49,10 +54,11 @@ class UserExistsView(APIView):
       User.objects.get(email=exist_query)
     except User.DoesNotExist:
     #   print('False')
-      return Response(data={'message': False}) # return false as user does not exist
+      # return false as user does not exist
+      return Response(data={'message': False})
     else:
     #   print('True')
-      return Response(data={'message': True}) # Otherwise, return True
+      return Response(data={'message': True})  # Otherwise, return True
       # if not User.objects.get(email=f'{exist_query}'):
       #   print('True')
       #   return Response(data={'message':True})
@@ -67,97 +73,111 @@ class UserExistsView(APIView):
       # # use this if username is being sent as a query parameter
       # email = self.request.query_params.get('email')
 
-
 # class UserViewCreate(CreateAPIView):
 #     queryset = User.objects.all()
-#     serializer_class = UserSerializer()
+#     serializer_class = UserSerializer
+#     # @permission_classes([AllowAny])
+# @api_view(['POST'])
+# def signup(request):
+#     serializer = UserSerializer(data=request.data)
+#     print(1111, serializer)
+#     # print(type(request.data))
+#     if serializer.is_valid():
+#         user = serializer.save()  # 최초 저장 후,
+#         # user.set_password(user.password)  # password 암호화하기 위해서 갈아끼운다.
+#         user.save()
+#         return Response(status=200, data={'message': '회원가입 성공'})
+#     else:
+#         print(serializer.error_messages)
+#         return Response(status=400, data={'message': serializer.error_messages})
 
-@api_view(['POST'])
-# @permission_classes([AllowAny])
-def signup(request):
-    serializer = UserSerializer(data=request.data)
-    print(1111, serializer)
-    # print(type(request.data))
-    if serializer.is_valid():
-        user = serializer.save()  # 최초 저장 후, 
-        # user.set_password(user.password)  # password 암호화하기 위해서 갈아끼운다. 
-        user.save()
-        return Response(status=200, data={'message': '회원가입 성공'}) 
-    else:
-        print(serializer.error_messages)
-        return Response(status=400, data={'message': serializer.error_messages}) 
+class CsrfExemptSessionAuthentication(authentication.SessionAuthentication):
+    def enforce_csrf(self, request):
+        return
+
+class UserViewCreate(APIView):
+    permission_classes = (permissions.AllowAny,)
+    authentication_classes = (CsrfExemptSessionAuthentication,)
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return response.Response(UserSerializer(user).data)
+
 
 class UserViewDetail(RetrieveAPIView):
-    lookup_field = 'id'
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    authentication_classes = (SessionAuthentication, BasicAuthentication)
-    permission_classes = (IsAuthenticated,)
+    lookup_field='id'
+    queryset=User.objects.all()
+    serializer_class=UserSerializer
+    authentication_classes=(SessionAuthentication, BasicAuthentication)
+    permission_classes=(IsAuthenticated,)
 
 
 class UserViewUpdate(UpdateAPIView):
-    lookup_field = 'id'
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    authentication_classes = (SessionAuthentication, BasicAuthentication)
-    permission_classes = (IsAuthenticated,)
+    lookup_field='id'
+    queryset=User.objects.all()
+    serializer_class=UserSerializer
+    authentication_classes=(SessionAuthentication, BasicAuthentication)
+    permission_classes=(IsAuthenticated,)
 
 
 class UserViewDelete(DestroyAPIView):
-    lookup_field = 'id'
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    authentication_classes = (SessionAuthentication, BasicAuthentication)
-    permission_classes = (IsAuthenticated,)
+    lookup_field='id'
+    queryset=User.objects.all()
+    serializer_class=UserSerializer
+    authentication_classes=(SessionAuthentication, BasicAuthentication)
+    permission_classes=(IsAuthenticated,)
 
 
 class HistroyViewCreate(CreateAPIView):
-    lookup_fields = 'user_id'
-    queryset = User_history.objects.all()
-    serializer_class = HistroySerializer
+    lookup_fields='user_id'
+    queryset=User_history.objects.all()
+    serializer_class=HistroySerializer
 
 
 class RankView(ListAPIView):
-    lookup_field = 'id'
+    lookup_field='id'
     # queryset = User_history.objects.all().order_by('total_paid')[:5]
-    queryset = User_history.objects.extra(
+    queryset=User_history.objects.extra(
     select={'fieldsum': 'user_lunch + user_dinner + user_breakfast'},
         order_by=('fieldsum',)
     )[:5]
-    serializer_class = HistroySerializer
+    serializer_class=HistroySerializer
 
 
 class HistroyView(ListAPIView):
-    lookup_field = 'user_id'
-    queryset = User_history.objects.all()
-    serializer_class = HistroySerializer
+    lookup_field='user_id'
+    queryset=User_history.objects.all()
+    serializer_class=HistroySerializer
 
     def get_queryset(self):
         return User_history.objects.filter(user_id=self.kwargs['user_id'])
 
 
 class HistoryViewDetail(RetrieveAPIView):
-    lookup_field = 'id'
-    queryset = User_history.objects.all()
-    serializer_class = HistroySerializer
+    lookup_field='id'
+    queryset=User_history.objects.all()
+    serializer_class=HistroySerializer
 
     def get_queryset(self):
         return User_history.objects.filter(id=self.kwargs['id'])
 
 
 class HistoryViewUpdate(UpdateAPIView):
-    lookup_field = 'user_id'
-    queryset = User_history.objects.all()
-    serializer_class = HistroySerializer
+    lookup_field='user_id'
+    queryset=User_history.objects.all()
+    serializer_class=HistroySerializer
 
     def get_queryset(self):
         return User_history.objects.filter(id=self.kwargs['id'], user_id=self.kwargs['user_id'])
 
 
 class HistroyViewDelete(DestroyAPIView):
-    lookup_field = 'user_id'
-    queryset = User_history.objects.all()
-    serializer_class = HistroySerializer
+    lookup_field='user_id'
+    queryset=User_history.objects.all()
+    serializer_class=HistroySerializer
 
     def get_queryset(self):
-        return User_history.objects.filter(id=self.kwargs['id'] , user_id=self.kwargs['user_id'])
+        return User_history.objects.filter(id=self.kwargs['id'], user_id=self.kwargs['user_id'])
