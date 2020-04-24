98269bf834cd70acd6ab7fe98c033a7ea9186289
jbh <ceajer@gmail.com>
2020-04-24 17:59:34 +0900

    0424 | Update sh file add migrate | BH

--- Back-End/whyEat/deploy_back.sh
+++ Back-End/whyEat/deploy_back.sh
@@ -9,6 +9,10 @@ else echo "> kill -15 $CURRENT_PID PID 종료"
         sudo kill -15 $CURRENT_PID
         sudo sleep 5
 fi
+if [ -f "$DB_FILE" ]; then
+    rm $DB_FILE
+fi
+python3 manage.py migrate
 
 echo "> Excute Django"
 
