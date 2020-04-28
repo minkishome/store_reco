766767cbdb7612bddeb5c8feb2ae2a7b4bdcd02f
jbh <ceajer@gmail.com>
2020-04-29 02:51:52 +0900

    0429 | Edit result algo | BH

--- Back-End/whyEat/stores/views.py
+++ Back-End/whyEat/stores/views.py
@@ -25,11 +25,9 @@ def store_list(request, kakao_id):
             'mysql+pymysql://root:1234@localhost/mydb', convert_unicode=True)
         conn = engine.connect()
         final_df = pd.read_sql_table('stores_store_score', conn)
-        # print(final_df.head)
         # final_df.rename(columns={'user_id': 'user'}, axis='columns')
         user_store_rating = final_df.pivot_table(
             'score', index='user_id', columns='store_name').fillna(0)
-        # print(user_store_rating.head())
         # matrix는 pivot_table 값을 numpy matrix로 만드는 것
         matrix = user_store_rating.as_matrix()
         # user_rating_mean 은 사용자의 평균 평점
@@ -37,6 +35,9 @@ def store_list(request, kakao_id):
         # matrix_user_mean 은 사용자-스토어에 대해 사용자 평균 평점을 뺀 것
         matrix_user_mean = matrix - user_ratings_mean.reshape(-1, 1)
         # 다시 df 처리
+        pd.DataFrame(matrix_user_mean,
+                     columns=user_store_rating.columns)
+
         # print(pd.DataFrame(matrix_user_mean, columns=user_store_rating.columns).head())
         # Python scipy에서 제공해주는 svd 사용
         # U행렬, sigma 행렬, V 전치행렬을 반환
@@ -58,8 +59,7 @@ def store_list(request, kakao_id):
         svd_user_predicted_ratings = np.dot(
             np.dot(U, sigma), Vt) + user_ratings_mean.reshape(-1, 1)
         df_svd_preds = pd.DataFrame(
-            svd_user_predicted_ratings, columns=user_store_rating.columns)
-        # print(df_svd_preds)
+            svd_user_predicted_ratings, index=user_store_rating.index, columns=user_store_rating.columns)
 
         def recommend_stores(df_svd_preds, user_id, ori_stores_df, ori_ratings_df, num_recommendations=5):
             print('here check')
@@ -69,33 +69,50 @@ def store_list(request, kakao_id):
             if not isempty:
                 print('여기는 들어있는거')
                 # 현재는 index로 적용 되어있는거라 -1 해줘야함
-                user_row_number = user_id-1
+                # user_row_number = ori_stores_df[ori_stores_df['user_id']
+                #                                 == user_id]['id']-1
+                user_row_number = user_id
+                # 여기가 수정부분
+                # user_row_number = ori_stores_df.loc[ori_stores_df['user_id'] == str(user_id), [
+                #     'id']]
+                # user_row_number = user_row_number.reset_index()
+
+                # print(user_row_number)
+                # print(type(user_row_number))
+                # user_row_number = user_row_number.loc[1]['id']
+                # print(type(user_row_number))
+                # print(user_row_number)
+                # 여기까지 수정부분 끝
 
                 # 최종적으로 만든 df_svd_preds 에서 사용자 index에 따라 스토어 데이터 정렬
-                sorted_user_predictions = df_svd_preds.iloc[user_row_number].sort_values(
+                sorted_user_predictions = df_svd_preds.loc[str(user_row_number)].sort_values(
                     ascending=False)
 
                 # 원본 평점 데이터에서 user id 에 해당하는 데이터를 뽑아낸다
-                user_data = ori_ratings_df[ori_ratings_df.user_id == user_id]
-                print(user_data)
+
+                user_data = ori_ratings_df[ori_ratings_df.user_id == str(
+                    user_id)]
                 # 위에서 뽑은 user_data와 원본 스토어 데이터를 합친다.
                 if not user_data.empty:
-                    user_history = user_data.merge(ori_stores_df, on='store_name').sort_values([
-                        'score'], ascending=False)
+                    print('제발')
+                    # user_history = user_data.merge(ori_stores_df, on='store_name').sort_values([
+                    #     'score'], ascending=False)
+                    user_history = user_data.merge(
+                        ori_stores_df, on='store_name')
                     # 원본 스토어 데이터에서 사용자가 평점 남긴 스토어를 제외한 데이터를 추출
                     recommendations = ori_stores_df[~ori_stores_df['store_name'].isin(
                         user_history['store_name'])]
 
                     # 사용자의 스토어 평점이 높은 순으로 정렬된 데이터와 위 추천 합친다
-
                     recommendations = recommendations.merge(pd.DataFrame(
                         sorted_user_predictions).reset_index(), on='store_name')
-
+                    print(recommendations.columns)
                     # 컬럼 이름 바꾸고 정렬해서 return
                     recommendations = recommendations.rename(
-                        columns={user_row_number: 'Predictions'}).sort_values('Predictions')
+                        columns={str(user_row_number): 'Predictions'}).sort_values('Predictions')
                 else:
                     print('여기는 empty')
+                    # 원본 스토어 데이터에서 사용자가 평점 남긴 스토어를 제외한 데이터를 추출
                     recommendations = ori_stores_df
                     # 사용자의 스토어 평점이 높은 순으로 정렬된 데이터와 위 추천 합친다
 
@@ -103,8 +120,9 @@ def store_list(request, kakao_id):
                         sorted_user_predictions).reset_index(), on='store_name')
 
                     # 컬럼 이름 바꾸고 정렬해서 return
+                    print(recommendations.columns)
                     recommendations = recommendations.rename(
-                        columns={user_row_number: 'Predictions'}).sort_values('Predictions')
+                        columns={str(user_id): 'Predictions'}, inplace=True).sort_values('Predictions')
 
                 return recommendations
             else:
