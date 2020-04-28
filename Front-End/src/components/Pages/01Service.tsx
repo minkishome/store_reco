a3028ff9890dd66750dba7ce745808e36351ec79
Youngsdg1 <52898735+Youngsdg1@users.noreply.github.com>
2020-04-28 15:37:17 +0900

    0428 | EDIT Link | YJ

--- Front-End/src/components/Pages/01Service.tsx
+++ Front-End/src/components/Pages/01Service.tsx
@@ -47,7 +47,7 @@ class Service extends Component<any, State> {
         sessionStorage.setItem('nickname', JSON.stringify(this.state.data.profile.properties.nickname));
         sessionStorage.setItem('password', '1234');
         alert('로그인되었습니다')
-        // window.location.href='http://13.125.68.151:8081/#fifthPage';
+        window.location.href='http://13.125.68.151:8081/#fifthPage';
       } else {
         try {
           const signup_response = await axios({
@@ -65,7 +65,7 @@ class Service extends Component<any, State> {
           });
           // 회원가입 -> Survey 연결 부분
           alert('회원가입 되었습니다.')
-          // window.location.href='http://13.125.68.151:8081/#thirdPage';
+          window.location.href='http://13.125.68.151:8081/#thirdPage';
         }
         catch (err) {
           sessionStorage.clear()
